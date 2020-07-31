f4155fd8d641d7738c114f4146461d651b430f41
Ran-n <ran-n@tutanota.com>
2020-07-31 23:30:22 +0200

    cambios

--- caixab.py
+++ caixab.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 #+ Autor:	Ran#
 #+ Creado:	28/07/2020 21:50:39
-#+ Editado:	28/07/2020 21:50:39
+#+ Editado:	31/07/2020 23:26:07
 
 import codigo.modelo.manexador_sqlite as sqlite
+import codigo.apis.coingeckoapi as cg
 
 vBD = sqlite.cBaseDatos('persoal')
 vBD.CrearBD()
+
+vCG = cg.cCoinGecko()
+valor_moedas = vCG.valor_moedas('phoneum', 'eur')
+print(valor_moedas)
+
+
---
+++ codigo/apis/coingeckoapi.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#+ Autor:	Ran#
+#+ Creado:	31/07/2020 22:55:28
+#+ Editado:	31/07/2020 23:28:04
+
+import requests as r
+
+class cCoinGecko:
+    def __init__(self):
+        self.url = 'https://api.coingecko.com/api/v3/'
+
+    def _conexion_controlada(self, resposta):
+        if resposta.ok:
+            return resposta.json()
+        else:
+            raise ValueError('Erro de conexión')
+
+    def ping(self):
+        url_ping = self.url+'ping'
+
+        return self._conexion_controlada(r.get(url_ping))
+
+    def lista_moedas_referencia(self):
+        url_moeda_ref = self.url+'simple/supported_vs_currencies'
+
+        return self._conexion_controlada(r.get(url_moeda_ref))
+
+    def lista_moedas(self):
+        url_moedas = self.url_base+'coins/list'
+
+        return self._conexion_controlada(r.get(url_moedas))
+
+    def valor_moedas(self,criptos,moedas):
+        url_valor_moedas = self.url+'simple/price?ids='+criptos+'&vs_currencies='+moedas
+
+        return self._conexion_controlada(r.get(url_valor_moedas))
+
